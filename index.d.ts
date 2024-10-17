2d6fe0465051d7d37eb629050ca18a127f6afc0f
prem-project3608 <144128294+prem-project3608@users.noreply.github.com>
2024-10-17 18:28:29 +0530

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,15 +1,3 @@
-/**
-
- *
-
- * @param {string} videoUrl - Facebook video URL (required)
-
- * @param {string} [cookie] - Facebook cookie (optional)
-
- * @param {string} [useragent] - User agent (optional)
-
- */
-
 export default function getVideoInfo(
 
     videoUrl: string,
